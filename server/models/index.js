2890331201b0c524a2653204d409db46b0158455
Em3L <martink_lopez@yahoo.com>
2023-10-19 21:06:27 +1100

    created started code for index js in models folder

---
+++ server/models/index.js
@@ -0,0 +1,6 @@
+// utilise proper variables for this project
+
+const User = require('');
+const Thought = require(''); // change Thought to COMMENT? we shall discuss
+
+module.exports = { User, Thought }; // change exports to proper models

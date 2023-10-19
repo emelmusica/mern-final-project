9a1bc85dd4141f7242d30c8b82566bb491a3d6a6
Em3L <martink_lopez@yahoo.com>
2023-10-19 21:56:25 +1100

    change thoughts to post

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 // utilise proper variables for this project
 
 const User = require('');
-const Thought = require(''); // change Thought to COMMENT? we shall discuss
+const Thought = require(''); // change Thought to POST? we shall discuss
 
 module.exports = { User, Thought }; // change exports to proper models
